adb4f7248cbe2a3c5410e7b995f903a26d5fd710
Whilmis Perez <Whilmis21@gmail.com>
2022-02-20 18:56:38 -0400

    docs agregado

---
+++ docs/precache-manifest.d8b911663a2d41348edee791483696e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3e9e71b4173e9168e2ba1921376f1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a9bbee06386d8b89786",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a9bbee06386d8b89786",
+    "url": "/static/js/main.ef426b5d.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
